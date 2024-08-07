a8c202063fe9cfaa533e92974285fb076ab5eb50
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-08-08 00:10:22 +0300

    Did the task-1 in the project

---
+++ src/basic/1.ts
@@ -0,0 +1,23 @@
+// JavaScript код
+
+// const age = 50;
+// const username = "Max";
+// const toggle = true;
+// const empty = null;
+// const callback = (a) => {
+//   return 100 + a;
+// };
+
+// TypeScript код
+
+const age: number = 50;
+const username: string = "Max";
+const toggle: boolean = true;
+const empty: null = null;
+const callback = (a: number): number => {
+  return 100 + a;
+};
+
+console.log(
+  `Types of variables: \nage: ${age}, \nusername: ${username}, \ntoggle: ${toggle}, \nempty: ${empty}, \ncallback: ${callback}`
+);
